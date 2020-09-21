9fb62c6a2550b36514c812049acb90d669840e45
IMHOJEONG <39ghwjd@naver.com>
2020-09-22 00:07:06 +0900

    Revise server.js

--- servers/server.js
+++ servers/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const path = require('path');
-
-app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../build')));
 
